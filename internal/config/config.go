e4bbc2b173dbb653c60461d1567dde6ce792e42f
Bastien PROMPSY <bastienprompsy@gmail.com>
2025-05-27 16:25:01 +0200

    copié tp precedetn

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type InputTarget struct {
+	Name  string `json:"name"`
+	URL   string `json:"url"`
+	Owner string `json:"owner"`
+}
+
+func LoadTargetsFromFile(filePath string) ([]InputTarget, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", filePath, err)
+	}
+	var targets []InputTarget
+	if err := json.Unmarshal(data, &targets); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON from file %s: %w", filePath, err)
+	}
+	return targets, nil
+}
+
+func SaveTargetsToFile(filePath string, targets []InputTarget) error {
+	data, err := json.MarshalIndent(targets, "", " ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal targets to JSON: %w", err)
+	}
+	if err := os.WriteFile(filePath, data, 0644); err != nil {
+		return fmt.Errorf("failed to write file %s: %w", filePath, err)
+	}
+	return nil
+}
